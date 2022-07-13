61848a424841a6c8d17922b88a38db2aceda1f00
Lany Aragão <lanyaragao5@gmail.com>
2022-07-13 16:48:46 -0300

    Adicionar condicional com cores de tabela

---
+++ AppEscola/Controllers/AlunoController.cs
@@ -0,0 +1,50 @@
+﻿using AppEscola.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppEscola.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            alunos.Add(new Aluno()
+            {
+                Id = 1,
+                Nome = "Willian",
+                Sobrenome = "Sant Anna",
+                Status = "Reprovado"
+            }) ;
+
+            alunos.Add(new Aluno()
+            {
+                Id = 2,
+                Nome = "Thiago",
+                Sobrenome = "Machado",
+                Status = "Aprovado"
+            });
+
+            alunos.Add(new Aluno()
+            {
+                Id = 3,
+                Nome = "Rodrigo",
+                Sobrenome = "Braga",
+                Status = "Aprovado"
+            });           
+            
+            alunos.Add(new Aluno()
+            {
+                Id = 4,
+                Nome = "Caroline",
+                Sobrenome = "Da Silva",
+                Status = "Recuperação"
+            });
+            
+            return View(alunos);
+        }
+    }
+}
+
+
+
+
---
+++ AppEscola/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace AppEscola.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Status { get; set; }
+    }
+}
