87d1a3ccc398b0b2859e3c65f9215892f3bc9ed0
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-07-14 06:49:15 -0300

    Criado a model Boletim e o Boletim Controller

---
+++ AppEscola/Controllers/BoletimController.cs
@@ -0,0 +1,54 @@
+﻿using AppEscola.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppEscola.Controllers
+{
+    public class BoletimController : Controller
+    {
+        public IActionResult Boletim()
+        {
+            List<Boletim> boletim = new List<Boletim>();
+            boletim.Add(new Boletim()
+            {
+                Materia = "Portugues",
+                Nota1 = 5,
+                Nota2 = 6.5,
+                Nota3 = 6.5,
+                Nota4 = 0,
+                Status = "Reprovado"
+            });
+
+            boletim.Add(new Boletim()
+            {
+                Materia = "Matemática",
+                Nota1 = 10,
+                Nota2 = 8,
+                Nota3 = 8,
+                Nota4 = 8,
+                Status = "Aprovado"
+            });
+
+            boletim.Add(new Boletim()
+            {
+                Materia = "Ciências",
+                Nota1 = 6,
+                Nota2 = 7.5,
+                Nota3 = 8,
+                Nota4 = 8,
+                Status = "Aprovado"
+            });
+
+            boletim.Add(new Boletim()
+            {
+                Materia = "História",
+                Nota1 = 8,
+                Nota2 = 9,
+                Nota3 = 10,
+                Nota4 = 7.5,
+                Status = "Aprovado"
+            });
+
+            return View(boletim);
+        }
+    }
+}
---
+++ AppEscola/Models/Boletim.cs
@@ -0,0 +1,15 @@
+﻿namespace AppEscola.Models
+{
+    public class Boletim
+    {   
+        public string Materia { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double Nota4 { get; set; }
+        public string Status { get; set; }
+
+
+
+    }
+}
